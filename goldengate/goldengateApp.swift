4a3576439a53b602571fab6815a3f2a6e88862c9
Anhelina Kruk <krukanhelina06@gmail.com>
2025-04-07 22:50:38 +0200

    Initial Commit

---
+++ goldengate/goldengateApp.swift
@@ -0,0 +1,17 @@
+//
+//  goldengateApp.swift
+//  goldengate
+//
+//  Created by Anhelina Kruk on 07/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct goldengateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
